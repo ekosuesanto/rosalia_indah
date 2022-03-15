ba7d9db4448c8eba22a5e3e7f8887570e9ed8c69
FlutLab.io <>
2022-03-15 06:35:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "rosaliaindahtransport/page/home_page.dart";
+import "package:rosaliaindahtransport/page/home_page.dart";
 void main(){
     runApp(const MyApps());
 }
