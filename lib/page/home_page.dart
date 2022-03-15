8537b239f26621ba6380afe30df8f8d6943a79c5
FlutLab.io <>
2022-03-15 07:12:52 +0000

    Autosave

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Image.network("https://upload.wikimedia.org/wikipedia/commons/5/5a/Logo_Rosalia_Indah.png")), body: Center(child: Text('hgfhvh')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.network(
+          "https://upload.wikimedia.org/wikipedia/commons/5/5a/Logo_Rosalia_Indah.png",
+          height: 50,
+        ),
+      ),
+      body: Center(
+        child: Text('hgfhvh'),
+      ),
+    );
   }
 }
